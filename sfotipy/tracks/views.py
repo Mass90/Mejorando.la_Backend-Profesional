a9ffa442b694c1191c0a5bf11136f35626b26242
Marco Antonio Sánchez Santos <mass90@Marcos-Mac-mini.local>
2014-04-20 16:30:01 -0500

    First part of the second class

--- sfotipy/tracks/views.py
+++ sfotipy/tracks/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+from .models import Track
+
+def track_view(request, title):
+#	try:
+#		track = Track.objects.get(title=title)
+#	except Track.DoesNotExist:
+#		raise Http404
+
+	#Esta linea hace todo lo que esta comentado
+	track = get_object_or_404(Track, title=title)
+	bio = track.artist.biography
+	
+	return render(request, 'tracks.html', {'track': track, 'bio': bio})
